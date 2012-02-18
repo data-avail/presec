02cd75c302a8a57095766388397d2a971835a0d8
Max Putilov <max.putilov@gmail.com>
2012-02-19 00:17:24 +0400

    forget include new nugets

---
+++ Presec/Service/Cahching/Cache.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Presec.Service.Models;
+using Enyim.Caching;
+using Enyim.Caching.Memcached;
+
+namespace Presec.Service.Cahching
+{
+    public class Cache<T> : ICache<T>
+    {
+        public Cache()
+        {}
+
+        private readonly MemcachedClient _client = new MemcachedClient();
+
+        public T Get(string term)
+        {
+            return _client.Get<T>(term);
+        }
+
+        public void Set(string term, T GeoSuggestion)
+        {
+            _client.Store(StoreMode.Add, term, GeoSuggestion);
+        }
+
+        public void Remove(string term)
+        {
+            _client.Remove(term);
+        }
+        
+
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
+    }
+}
---
+++ Presec/Service/Cahching/ICache.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Presec.Service.Cahching
+{
+    public interface ICache<T> : IDisposable
+    {
+        void Set(string key, T obj);
+
+        T Get(string key);
+
+        void Remove(string Key);
+    }
+}

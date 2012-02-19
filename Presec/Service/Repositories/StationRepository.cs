2ffe1e7f2c79b451ce886cf7ff624f1c2eb06592
Max Putilov <max.putilov@gmail.com>
2012-02-19 19:30:09 +0400

    parse addr string

--- Presec/Service/Repositories/StationRepository.cs
+++ Presec/Service/Repositories/StationRepository.cs
@@ -15,6 +15,11 @@ namespace Presec.Service.Repositories
 {
     public class StationRepository
     {
+        private static string[] routeNames = new[] { "улица", "площадь", "набережная", "проспект", "тупик", "шоссе", "проезд", "наб.", "ул.", "пр.", "ш.", "пл." };
+        private static string[] routeNamesWell = new[] { "улица", "площадь", "набережная", "проспект", "тупик", "шоссе", "проезд", "набережная", "улица", "проезд", "шоссе", "площадь" };
+        private static string[] premiseNames = new[] { "дом", "строение", "корпус", "здание", "стр.", "к.", "д." };
+        private static string[] premiseNamesWell = new[] { "дом", "строение", "корпус", "здание", "строение", "корпус", "дом" };
+
         public Station GetOne(string id)
         {
             return new Station { id = 0 };
@@ -26,19 +31,19 @@ namespace Presec.Service.Repositories
 
             if (!string.IsNullOrEmpty(addr))
             {
-                var spts = addr.Split(',');
+                var spts = GetAddr(addr);
 
                 string regex = null;
 
-                if (spts.Count() == 1)
+                if (spts[1] == null)
                 {
-                    regex = string.Format(".*{0}.*", spts[0].Trim());
+                    regex = string.Format(".*{0}.*", spts[0].Split('|')[1]);
                 }
                 else
                 {
-                    regex = string.Format(".*{0}^d*{1}^d.*", spts[0], spts[1]);
+                    regex = string.Format(@".*{0}.*{1}.*", spts[0].Split('|')[1], spts[1].Split('|')[1]);
                 }
-
+                
 
                 //Since Norm doesn't support nested spatials - use native driver
 
@@ -94,5 +99,89 @@ namespace Presec.Service.Repositories
 
             return new Station[0];
         }
+
+        public string[] GetAddr(string RawAddr)
+        {
+            // var regexStr = @"^([\d]*?[\w\s]+?[^\d])([\w\.\,]*[^\d])([\d]+.*)$";
+            var regexStr = @"^([\d]*?\b[^\d]+)\b(\d+.*)";
+
+            //find in clenead address - route, then premise, (premise contains any number + к or c)
+            var matches = Regex.Matches(RawAddr, regexStr, RegexOptions.IgnoreCase);
+
+            //if matches.cnt = 0 consider string consists only from route part
+            //else
+            //1st - route part + type
+            //2nd - premise type or part of the 1st
+            //3d - premise part
+
+            string routePart = RawAddr;
+            string premisePart = null;
+            if (matches.Count > 0)
+            {
+                routePart = matches[0].Groups[1].Value.Trim();
+                premisePart = matches[0].Groups[2].Value.Trim();
+                string premisePartWell = null;
+                var i = routePart.LastIndexOf(' ');
+                if (i != -1)
+                {
+                    var mabyRoutePartContainsPremiseType = routePart.Substring(i).Trim();
+                    premisePartWell = GetPremiseWell(mabyRoutePartContainsPremiseType);
+                }
+                if (premisePartWell != null)
+                {
+                    routePart = routePart.Remove(i);
+                    premisePart = premisePartWell + " " + premisePart;
+                }
+            }
+
+            var routeRegex = string.Format(@"^(.*)\b({0})\s?(.*)", string.Join("|", routeNames.Select(p=>p.Replace(".",@"\.?"))));
+
+            matches = Regex.Matches(routePart, routeRegex, RegexOptions.IgnoreCase);
+            if (matches.Count > 0)
+            {
+                var well = GetRouteWell(matches[0].Groups[2].Value);
+                
+                routePart = Regex.Replace(routePart, routeRegex, "$1$3", RegexOptions.IgnoreCase).Trim();
+                routePart = well + "|" + routePart;
+            }
+            else
+            {
+                routePart = "|" + routePart;
+            }
+
+            if (premisePart != null)
+            {
+                var premiseRegex = string.Format(@"^(.*)\b({0})\s?(.*)", string.Join("|", premiseNames.Select(p => p.Replace(".", @"\.?"))));
+
+                matches = Regex.Matches(premisePart, premiseRegex, RegexOptions.IgnoreCase);
+                if (matches.Count > 0)
+                {
+                    var well = GetPremiseWell(matches[0].Groups[2].Value);
+                    premisePart = Regex.Replace(premisePart, premiseRegex, "$1$3", RegexOptions.IgnoreCase).Trim();
+                    premisePart = well + "|" + premisePart;
+                }
+                else
+                {
+                    premisePart = "|" + premisePart;
+                }                
+            }
+
+            return new string[] { routePart.ToLower().Trim(), premisePart != null ? premisePart.ToLower().Trim() : null};
+        }
+
+        private string GetRouteWell(string Route)
+        {
+            var i = routeNames.Select(p => p.Trim('.')).ToList().IndexOf(Route.Trim('.'));
+
+            return i != -1 ? routeNamesWell.ToArray()[i] : null;
+        }
+
+        private string GetPremiseWell(string Premise)
+        {
+            var i = premiseNames.Select(p => p.Trim('.')).ToList().IndexOf(Premise.Trim('.'));
+
+            return i != -1 ? premiseNamesWell.ToArray()[i] : null;
+        }
+
     }
 }
